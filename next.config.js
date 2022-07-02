c10e33f0815b7ec52fe956560b009c3c3be4b49e
Leandro Guina <guina86@gmail.com>
2022-07-02 10:44:57 -0300

    add storybook

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
 
-const nextConfig = {
+const nextConfig = withPWA({
+  swcMinify: true,
   reactStrictMode: true,
   compiler: {
-    styledComponents: {
-      ssr: true
-    }
+    styledComponents: true
+  },
+  pwa: {
+    dest: 'public',
+    disable: !isProd
   }
-}
+})
 
 module.exports = nextConfig
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -24,7 +24,7 @@ export const Title = styled.h1`
 
 export const Description = styled.h2`
   font-size: 2rem;
-  font-weight: bold;
+  font-weight: 400;
 `
 
 export const Illustration = styled.img`
